cbf54248e3e5693815cb256458f41c49dbfd3949
Daoud Clarke <daoud.clarke@gmail.com>
2014-07-25 09:03:02 +0100

    Add file containing inference code

---
+++ ldacomp/inference.py
@@ -0,0 +1,22 @@
+# Bismillahi-r-Rahmani-r-Rahim
+#
+# Compute probabilities of documents
+
+
+from gensim.models.ldamodel import LdaModel
+from gensim.corpora import Dictionary
+
+import settings
+
+
+class InferenceModel(object):
+    def __init__(self):
+        self.model = LdaModel.load(settings.lda_model_name)
+        self.dictionary = Dictionary.load_from_text(settings.wordids_txt)
+        
+    def prob(self, words):
+        bow = self.dictionary.doc2bow(words)
+        if len(bow) != len(words):
+            print "One or more words missing from from dictionary"
+        return self.model.inference([bow])
+
